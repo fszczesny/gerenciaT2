0adf895acb4fe564468de1398aa86a7b441e0ed4
Fabricio Almeida <fabricio.sda@gmail.com>
2019-11-16 17:17:16 -0300

    Modularizando codigo do client.py

--- src/gerenciamento/cliente_container/client.py
+++ src/gerenciamento/cliente_container/client.py
@@ -6,27 +6,37 @@ from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 import threading
 
-client = iperf3.Client()
-client.duration = 15
-client.server_hostname = '192.168.0.109'
-client.port = 5201
+# Cria e estabelece conexao
+def startClient(ip, port, connectionDuration):
+    client = iperf3.Client()
+    client.duration = connectionDuration
+    client.server_hostname = ip
+    client.port = port
+    print('[INFO] - Connecting to {0}:{1}'.format(client.server_hostname, client.port))
+    result = client.run()
+    return result
 
-print('[INFO] - Connecting to {0}:{1}'.format(client.server_hostname, client.port))
-result = client.run()
+# Analisa conexao
+def analiseConnection(result):
+    # Dados no formato [taxa de transmissao, taxa de retransmissao, % de uso de cpu no dispositivo monitorado]
+    data = []
+    if result.error:
+        return data
+    else:
+        print('')
+        print('Test completed:')
+        print('  bytes transmitted  {0}'.format(result.sent_bytes))
+        print('  retransmits        {0}'.format(result.retransmits))
+        print('  avg cpu load       {0}%'.format(result.local_cpu_total))
+        print('  Transmitted data in Megabits per second  (Mbps)  {0}'.format(result.sent_Mbps))
+        data.append(float(result.sent_Mbps))
+        data.append(float(result.retransmits/result.sent_bytes))
+        data.append(float(result.local_cpu_total))
+        return data
 
-if result.error:
-    print(result.error)
-else:
-    print('')
-    print('Test completed:')
-    print('  started at         {0}'.format(result.time))
-    print('  bytes transmitted  {0}'.format(result.sent_bytes))
-    print('  retransmits        {0}'.format(result.retransmits))
-    print('  avg cpu load       {0}%\n'.format(result.local_cpu_total))
-
-    print('Average transmitted data in all sorts of networky formats:')
-    print('  bits per second      (bps)   {0}'.format(result.sent_bps))
-    print('  Kilobits per second  (kbps)  {0}'.format(result.sent_kbps))
-    print('  Megabits per second  (Mbps)  {0}'.format(result.sent_Mbps))
-    print('  KiloBytes per second (kB/s)  {0}'.format(result.sent_kB_s))
-    print('  MegaBytes per second (MB/s)  {0}'.format(result.sent_MB_s))
+ipDevice = '192.168.0.109'
+portDevice = 5201
+connectionTime = 15
+retunedData = []
+connectionResut = startClient(ipDevice, portDevice, connectionTime)
+returnedData = analiseConnection(connectionResut)
