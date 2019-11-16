94f5b2e4fb5f56a65aacf81afcadb874ea378015
Fabricio Almeida <fabricio.sda@gmail.com>
2019-11-16 18:02:35 -0300

    Preparando o backend - Há Pendencias
    Pendencias
    > Realizar app routes

--- src/gerenciamento/cliente_container/client.py
+++ src/gerenciamento/cliente_container/client.py
@@ -30,13 +30,73 @@ def analiseConnection(result):
         print('  avg cpu load       {0}%'.format(result.local_cpu_total))
         print('  Transmitted data in Megabits per second  (Mbps)  {0}'.format(result.sent_Mbps))
         data.append(float(result.sent_Mbps))
-        data.append(float(result.retransmits/result.sent_bytes))
+        data.append(float((result.retransmits/result.sent_bytes)*100))
         data.append(float(result.local_cpu_total))
         return data
 
-ipDevice = '192.168.0.109'
+connectionResut = False
+lock = threading.Lock()
+ipDevice = ''
 portDevice = 5201
 connectionTime = 15
-retunedData = []
+minNetworkBand = 1  # Valor em Mbps
+maxNetworkLoss = 20 # Valor em porcentagem
+maxDeviceCpu = 90   # Valor em porcentagem
+networkBand = 0     # Valor em Mbps
+networkLoss = 0     # Valor em porcentagem
+deviceCpu = 0       # Valor em porcentagem
+alertBand = False
+alertLoss = False
+alertCpu = False
+
+def updateVariables():
+    while (True):
+        lock.acquire()
+        global portDevice
+        global ipDevice
+        global connectionTime
+        global networkBand
+        global networkLoss
+        global deviceCpu
+        global minNetworkBand
+        global maxNetworkLoss
+        global maxDeviceCpu
+        global alertBand
+        global alertLoss
+        global alertCpu
+
+        retunedData = []
+
+        if connectionResut is not False:
+            returnedData = analiseConnection(connectionResut)
+            if (len(retunedData) == 3)
+                networkBand = retunedData[0]
+                networkLoss = retunedData[1]
+                deviceCpu = retunedData[2]
+                if (networkBand < minNetworkBand)
+                    alertBand = True
+                else
+                    alertBand = False
+                if (networkLoss > maxNetworkLoss)
+                    alertBand = True
+                else
+                    alertBand = False
+                if (deviceCpu > maxDeviceCpu)
+                    alertBand = True
+                else
+                    alertBand = False
+
+        lock.release()
+        time.sleep(5)
+
+updateVariablesThread = threading.Thread(target=updateVariables)
+updateVariablesThread.start()
+
+app = Flask(__name__)
+
+cors = CORS(app)
+
+# Aqui tem que criar os apps routes
 connectionResut = startClient(ipDevice, portDevice, connectionTime)
-returnedData = analiseConnection(connectionResut)
+
+updateVariablesThread.join()
